8130a0fe5e9094f3048a3bb939e1883aa7ce2c67
MukizuL <53896777+MukizuL@users.noreply.github.com>
2026-01-22 21:11:42 +0700

    Added TLS

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"greep-api-test/internal/config"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
@@ -12,9 +14,17 @@ import (
 )
 
 func newHTTPServer(lc fx.Lifecycle, cfg *config.Config, router *gin.Engine, logger *zap.Logger) *http.Server {
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-		Addr:    cfg.Addr,
-		Handler: router.Handler(),
+		Addr:         cfg.Addr,
+		Handler:      router.Handler(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	lc.Append(fx.Hook{
@@ -24,7 +34,7 @@ func newHTTPServer(lc fx.Lifecycle, cfg *config.Config, router *gin.Engine, logg
 			var err error
 
 			go func() {
-				err = srv.ListenAndServe()
+				err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 			}()
 
 			if err != nil && !errors.Is(err, http.ErrServerClosed) {
